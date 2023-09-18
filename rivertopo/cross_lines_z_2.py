92b67102a028bee917448692f4f17800aec8bc7c
Freja Froberg <frfro@sdfi.dk>
2023-09-18 14:06:21 +0200

    sorting of points and segments

--- rivertopo/cross_lines_z_2.py
+++ rivertopo/cross_lines_z_2.py
@@ -3,9 +3,12 @@ from osgeo import gdal, ogr
 import numpy as np
 import argparse
 import logging
+from itertools import groupby
+from operator import itemgetter
 
-from rivertopo.profile import RegulativProfilSimpel, RegulativProfilSammensat, OpmaaltProfil # import interpolation classes
-from rivertopo.snapping import snap_points
+
+from profile import RegulativProfilSimpel, RegulativProfilSammensat, OpmaaltProfil # import interpolation classes
+from snapping import snap_points
 
 gdal.UseExceptions()
 ogr.UseExceptions()
@@ -100,7 +103,7 @@ def give_profile_to_segments(segment_linestring, points_with_profiles):
     closest_point = None
     profile_type_line = None
     for point_with_profile in points_with_profiles:
-        point_feature, profile_type = point_with_profile
+        point_feature, profile_type, laengdeprofillokalid, regulativstationering = point_with_profile
         point = point_feature.GetGeometryRef().Clone()
 
         #snapping operation
@@ -152,16 +155,16 @@ def create_lines_from_perp_lines(line_profiles, offset, t, x3, x4, y3, y4, previ
 def main():
     argument_parser = argparse.ArgumentParser()
     argument_parser.add_argument('input_points_simpel', type=str, help='input points vector data source for simpel')
-    argument_parser.add_argument('input_points_sammensat', type=str, help='input points vector data source for sammensat')
-    argument_parser.add_argument('input_points_opmaalt', type=str, help= 'input points vector data source for opmaalt')
+    #argument_parser.add_argument('input_points_sammensat', type=str, help='input points vector data source for sammensat')
+    #argument_parser.add_argument('input_points_opmaalt', type=str, help= 'input points vector data source for opmaalt')
     argument_parser.add_argument('input_polyline', type=str, help='input polyline vector data source')
     argument_parser.add_argument('output_lines', type=str, help='output geometry file for lines with Z')
 
     input_arguments = argument_parser.parse_args()
 
     input_points_simpel_path = input_arguments.input_points_simpel
-    input_points_sammensat_path = input_arguments.input_points_sammensat
-    input_points_opmaalt_path = input_arguments.input_points_opmaalt
+    #input_points_sammensat_path = input_arguments.input_points_sammensat
+    #input_points_opmaalt_path = input_arguments.input_points_opmaalt
     input_polyline_path = input_arguments.input_polyline
     output_lines_path = input_arguments.output_lines
 
@@ -170,24 +173,49 @@ def main():
     input_polyline_layer = input_polyline_datasrc.GetLayer()
     input_polyline_feature = input_polyline_layer.GetNextFeature()
 
-    segments = []
+    segments_dict = {}
     for input_polyline_feature in input_polyline_layer:
         stream_linestring = input_polyline_feature.GetGeometryRef()
 
         linestring_points = np.array(stream_linestring.GetPoints())[:,:2]
+        laengdeprofillokalid = input_polyline_feature.GetField('laengdeprofillokalid')
+        sort_value = input_polyline_feature.GetField('sort')
+        print('printing laengdeprofilloaklid:',laengdeprofillokalid)
 
+        if laengdeprofillokalid not in segments_dict:
+            segments_dict[laengdeprofillokalid] = []
+    
+        # if laengdeprofillokalid not in segments:
+        #     segments[laengdeprofillokalid] = []
+    
+        # for i in range(len(linestring_points) - 1):
+        #     segment = (linestring_points[i], linestring_points[i+1])
+        #     segments.append(segment)
+    
         for i in range(len(linestring_points) - 1):
             segment = (linestring_points[i], linestring_points[i+1])
-            segments.append(segment)
-       
+            segments_dict[laengdeprofillokalid].append((sort_value,segment))
+        
+    # Sort segments within each laengdeprofillokalid group based on the sort attribute
+    for laengdeprofillokalid, segments in segments_dict.items():
+        segments.sort(key=lambda x: x[0])
+        segments_dict[laengdeprofillokalid] = [segment for sort_value, segment in segments]
+        
     points = []
-    for input_points_path, profile_type in [(input_points_simpel_path, 'RegulativProfilSimpel'), (input_points_sammensat_path, 'RegulativProfilSammensat'), (input_points_opmaalt_path, 'OpmaaltProfil')]:
+    for input_points_path, profile_type in [(input_points_simpel_path, 'RegulativProfilSimpel')]: #, (input_points_sammensat_path, 'RegulativProfilSammensat'), (input_points_opmaalt_path, 'OpmaaltProfil')]:
         input_points_datasrc = ogr.Open(input_points_path)
         input_points_layer = input_points_datasrc.GetLayer()
 
         for point_feature in input_points_layer:
+            laengdeprofillokalid = point_feature.GetField('laengdeprofillokalid')
+            regulativstationering = point_feature.GetField('regulativstationering')
             point_feature.profile_type = profile_type
-            points.append((point_feature, profile_type))
+            points.append((point_feature, profile_type, laengdeprofillokalid, regulativstationering))
+    
+    # Group points by 'laengdeprofillokalid' and sort them by 'regulativstationering'
+    points_sorted = sorted(points, key=itemgetter(3))  # Sort by 'regulativstationering'
+    points_grouped = groupby(points_sorted, key=itemgetter(2))  # Group by 'laengdeprofillokalid'
+
     
     #create the output file
     output_lines_driver = ogr.GetDriverByName("gpkg")
@@ -202,27 +230,47 @@ def main():
     #create lists to store perpendicular lines in
     previous_perpendicular_line = None
 
-    # Loop over all segments
-    for segment in segments:
-        #convert segment to LineString
-        segment_linestring = ogr.Geometry(ogr.wkbLineString)
-        segment_linestring.AddPoint(*segment[0])
-        segment_linestring.AddPoint(*segment[1])
-
-        #find the closest point and its profile for each segment
-        closest_point, profile_type_line = give_profile_to_segments(segment_linestring, points)
-
-        perpendicular_lines, offset, t, x3, x4, y3, y4 = create_perpendicular_lines_on_polylines(segment_linestring, length=10, interval=1)
-        
-        #associate each perpendicular line with a profile
-        line_profiles = []
-        for perp_line in perpendicular_lines:
-            line_profiles.append((perp_line, get_profile(closest_point, profile_type_line)))
-        
-        profiles = get_profile(closest_point, profile_type_line)
-
-        previous_perpendicular_line = create_lines_from_perp_lines(profiles, offset, t, x3, x4, y3, y4, previous_perpendicular_line, output_lines_layer)
+    # Loop over all groups
+    for laengdeprofillokalid, segments in segments_dict.items():
+        previous_perpendicular_line = None
     
+        # Define which segments belong to which 'laengdeprofillokalid'
+        if laengdeprofillokalid == '{0ED96FD8-94D4-4550-912C-2C38ADCCCF8F}':
+            segment_indices = [0, 1, 2, 3]  
+        elif laengdeprofillokalid == '{E368FFCE-965B-46E8-9DFE-40D682B6C7E2}':
+            segment_indices = [4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]  
+        else:
+            segment_indices = []  
+
+        for segment_index in segment_indices:
+            segment = segments[segment_index]
+
+
+        # Loop over all segments within the current group
+        for segment_index in segment_indices:
+            segment = segments[segment_index]
+
+            # Loop over all segments
+            #for segment in segments:
+                #convert segment to LineString
+            segment_linestring = ogr.Geometry(ogr.wkbLineString)
+            segment_linestring.AddPoint(*segment[0])
+            segment_linestring.AddPoint(*segment[1])
+
+            #find the closest point and its profile for each segment
+            closest_point, profile_type_line = give_profile_to_segments(segment_linestring, points)
+
+            perpendicular_lines, offset, t, x3, x4, y3, y4 = create_perpendicular_lines_on_polylines(segment_linestring, length=10, interval=1)
+                
+            #associate each perpendicular line with a profile
+            line_profiles = []
+            for perp_line in perpendicular_lines:
+                line_profiles.append((perp_line, get_profile(closest_point, profile_type_line)))
+                
+            profiles = get_profile(closest_point, profile_type_line)
+
+            previous_perpendicular_line = create_lines_from_perp_lines(profiles, offset, t, x3, x4, y3, y4, previous_perpendicular_line, output_lines_layer)
+            
         
     logging.info(f"processed {len(points)} points and created lines")
 
