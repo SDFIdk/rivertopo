554390f285fd894cbffa823da771bc9e2677fa86
Peter Limkilde Svendsen <pelis@sdfi.dk>
2023-11-30 14:15:32 +0100

    Use pyproject.toml for packaging

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(
-    name="rivertopo",
-    description="Tools to help combine river profile data and DEMs",
-    license="MIT",
-    author="Danish Agency for Data Supply and Infrastructure (SDFI)",
-    author_email="sdfi@sdfi.dk",
-)
